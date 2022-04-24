c2c6dec82a7c2cf3ca32646c17d417cb662c3538
Tatyana <android87@list.ru>
2022-04-24 20:48:44 +0400

    made if Masha 2

---
+++ example005_If_Masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("write your name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha") // .ToLower() переводит все символы в нижний регистр, как в примере
+{
+    Console.WriteLine("wow it's MASHA!!!");
+}
+else
+{
+    Console.Write("hi ");
+    Console.WriteLine(username);
+}
