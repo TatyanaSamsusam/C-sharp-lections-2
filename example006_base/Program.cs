a2716712b63e71edfe05b68d48a13c40f23b30b3
Tatyana <android87@list.ru>
2022-04-24 20:58:45 +0400

    look for max primitiv

---
+++ example006_base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 3;
+int c = 10;
+int d = 5;
+int e = 6;
+
+int max = a;
+
+if(b> max) max = b;
+if(c> max) max = c;
+if(d> max) max = d;
+if(e> max) max = e;
+
+Console.Write("max is ");
+Console.WriteLine(max);
