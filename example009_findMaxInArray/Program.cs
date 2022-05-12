03156030a237c6eb3380666647bd3dba0267fb95
Tatyana <android87@list.ru>
2022-05-12 17:03:08 +0400

    find max in array

---
+++ example009_findMaxInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {18,21,13,44,85,66,17,89,49};
+
+int findMax(int arg1, int arg2, int arg3)    //ввела функцию findMax c тремя аргументами
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int max = findMax(
+    findMax(array[0], array[1], array[2]),
+    findMax(array[3], array[4], array[5]),
+    findMax(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
