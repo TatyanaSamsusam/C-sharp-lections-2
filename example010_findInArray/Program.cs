6bad5be1bc12c1f79cdce13d318bd1d758dc7655
Tatyana <android87@list.ru>
2022-05-17 21:20:27 +0400

    find index of requested number in array

---
+++ example010_findInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// покажи, под каким индексом стоит число, равное find, в одномерном массиве array с n элементов
+
+int[] array = {18,21,13,44,85,66,17,89,49,18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1; //index ++
+}
